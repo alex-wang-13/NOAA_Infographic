937ee175cf4165de4e8ade41fa5808bb140b3c97
alex-wang-13 <106714485+alex-wang-13@users.noreply.github.com>
2023-04-24 16:16:09 -0400

    Added months to chart

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const outerRadius = innerRadius * 3;
 let url =
   'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/globe/ocean/all/1/1900-2023/data.json';
 
-$.getJSON(url, function (data) {
+$.getJSON(url, function (rawData) {
+  const description = rawData['description'];
+  const data = rawData['data'];
   console.log(data);
+
   // Get the canvas
   var canvas = $('#graph')[0];
   var ctx = canvas.getContext('2d');
@@ -38,8 +41,51 @@ $.getJSON(url, function (data) {
   ctx.beginPath();
   ctx.arc(canvasHeight / 2, canvasWidth / 2, outerRadius, 0, 2 * Math.PI, true);
   ctx.stroke();
+
+  // Making month labels
+  ctx.font = outerRadius * 0.1 + 'px arial';
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  const months = [
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+    'Jan',
+    'Feb',
+  ];
+  months.forEach((month, index) => {
+    // Save the original position of the Canvas
+    ctx.save();
+    // Center Canvas
+    ctx.translate(canvasWidth / 2, canvasHeight / 2);
+    // Rotate and fill text
+    var angle = (index * Math.PI) / 6;
+    ctx.rotate(angle);
+    ctx.fillText(month, outerRadius * 1.2, 0);
+    // Restore the saved state of the Canvas
+    ctx.restore();
+  });
+
+  // Iterate through the values in the data
+  Object.values(data).forEach((value, index) => {
+    ctx.save();
+    ctx.translate(canvasWidth / 2, canvasHeight / 2);
+    var angle = (index * Math.PI) / 6;
+    ctx.rotate(angle);
+
+    ctx.restore();
+  });
 });
 
 // Color backgrounds
-$('#title').css('background-color', '#08FFFF');
-$('#graph').css('background-color', '#DDDDDDDD');
+$('#title').css('background-color', 'dimgray');
+//$('#graph').css('background-color', '#DDDDFF');
+
+$("body").ready().css("background-color", "darkslategray");
