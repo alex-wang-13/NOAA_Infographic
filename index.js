f2e6659f3711b59f39088110361dea20676cf7ea
alex-wang-13 <106714485+alex-wang-13@users.noreply.github.com>
2023-04-24 09:55:49 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 // Import stylesheets
 import './style.css';
 
+const canvas = $('#graph');
+const canvasHeight = canvas.height();
+const canvasWidth = canvas.width();
+const innerRadius =
+  canvasHeight < canvasWidth ? canvasHeight / 8 : canvasWidth / 8;
+const centerRadius = innerRadius * 2;
+const outerRadius = innerRadius * 3;
+
 let url =
   'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/globe/ocean/all/1/1900-2023/data.json';
 
 $.getJSON(url, function (data) {
   console.log(data);
+  // Get the canvas
+  var canvas = $('#graph')[0];
+  var ctx = canvas.getContext('2d');
+
+  // Draw inner radius
+  ctx.arc(canvasHeight / 2, canvasWidth / 2, innerRadius, 0, 2 * Math.PI, true);
+  ctx.stroke();
+
+  // Draw center radius
+  ctx.beginPath();
+  ctx.arc(
+    canvasHeight / 2,
+    canvasWidth / 2,
+    centerRadius,
+    0,
+    2 * Math.PI,
+    true
+  );
+  ctx.stroke();
+
+  // Draw outer radius
+  ctx.beginPath();
+  ctx.arc(canvasHeight / 2, canvasWidth / 2, outerRadius, 0, 2 * Math.PI, true);
+  ctx.stroke();
 });
 
 $('#title').css('background-color', '#08FFFF').text('Title');
